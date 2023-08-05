5cc8e983215a420d056b329bfc01fb3d3b5bf29e
sxyazi <sxyazi@gmail.com>
2023-08-05 17:25:26 +0800

    feat: improve the performance of highlighting big JSON file

--- core/src/external/jq.rs
+++ core/src/external/jq.rs
@@ -1,19 +1,32 @@
-use std::path::Path;
+use std::{path::Path, process::Stdio};
 
 use anyhow::{bail, Result};
 use config::PREVIEW;
-use tokio::process::Command;
+use tokio::{io::{AsyncBufReadExt, BufReader}, process::Command};
 
-pub async fn jq(path: &Path) -> Result<String> {
-	let output = Command::new("jq")
+pub async fn jq(path: &Path, mut lines: usize) -> Result<String> {
+	let mut child = Command::new("jq")
 		.args(["-C", "--indent", &PREVIEW.tab_size.to_string(), "."])
 		.arg(path)
+		.stdout(Stdio::piped())
+		.stderr(Stdio::null())
 		.kill_on_drop(true)
-		.output()
-		.await?;
+		.spawn()?;
 
-	if !output.status.success() {
-		bail!("failed to get json: {}", String::from_utf8_lossy(&output.stderr));
+	let mut it = BufReader::new(child.stdout.take().unwrap()).lines();
+	let mut output = String::new();
+	while let Ok(Some(line)) = it.next_line().await {
+		if lines < 1 {
+			break;
+		}
+
+		output.push_str(&line);
+		output.push('\n');
+		lines -= 1;
 	}
-	Ok(String::from_utf8_lossy(&output.stdout).trim().to_string())
+
+	if output.is_empty() {
+		bail!("failed to get head of jq");
+	}
+	Ok(output)
 }
