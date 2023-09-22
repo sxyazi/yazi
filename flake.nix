774937e109c7ad590714d5155290649aecb5166b
XYenon <i@xyenon.bid>
2023-09-22 15:40:50 +0800

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        yazi = pkgs.callPackage ./nix/yazi.nix { };
+      in
+      {
+        packages.default = yazi;
+        packages.yazi = yazi;
+
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShells.default = import ./nix/shell.nix { inherit pkgs inputs; };
+      });
+}
