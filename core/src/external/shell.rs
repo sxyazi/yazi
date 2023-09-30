101d0fe13f2c8fdf5c3222164d0516020591fe11
sxyazi <sxyazi@gmail.com>
2023-09-30 17:17:53 +0800

    fix: set stdio to null when orphan is true

--- core/src/external/shell.rs
+++ core/src/external/shell.rs
@@ -15,37 +15,44 @@ impl ShellOpt {
 		self.piped = true;
 		self
 	}
+
+	#[inline]
+	fn stdio(&self) -> Stdio {
+		if self.orphan {
+			Stdio::null()
+		} else if self.piped {
+			Stdio::piped()
+		} else {
+			Stdio::inherit()
+		}
+	}
 }
 
 pub fn shell(opt: ShellOpt) -> Result<Child> {
-	#[cfg(not(target_os = "windows"))]
-	{
-		Ok(
-			Command::new("sh")
-				.arg("-c")
-				.arg(opt.cmd)
-				.arg("") // $0 is the command name
-				.args(opt.args)
-				.stdin(if opt.piped { Stdio::piped() } else { Stdio::inherit() })
-				.stdout(if opt.piped { Stdio::piped() } else { Stdio::inherit() })
-				.stderr(if opt.piped { Stdio::piped() } else { Stdio::inherit() })
-				.kill_on_drop(!opt.orphan)
-				.spawn()?,
-		)
-	}
+	#[cfg(unix)]
+	return Ok(
+		Command::new("sh")
+			.arg("-c")
+			.stdin(opt.stdio())
+			.stdout(opt.stdio())
+			.stderr(opt.stdio())
+			.arg(opt.cmd)
+			.arg("") // $0 is the command name
+			.args(opt.args)
+			.kill_on_drop(!opt.orphan)
+			.spawn()?,
+	);
 
-	#[cfg(target_os = "windows")]
-	{
-		Ok(
-			Command::new("cmd")
-				.arg("/C")
-				.arg(opt.cmd)
-				.args(opt.args)
-				.stdin(if opt.piped { Stdio::piped() } else { Stdio::inherit() })
-				.stdout(if opt.piped { Stdio::piped() } else { Stdio::inherit() })
-				.stderr(if opt.piped { Stdio::piped() } else { Stdio::inherit() })
-				.kill_on_drop(true)
-				.spawn()?,
-		)
-	}
+	#[cfg(windows)]
+	return Ok(
+		Command::new("cmd")
+			.stdin(opt.stdio())
+			.stdout(opt.stdio())
+			.stderr(opt.stdio())
+			.arg("/C")
+			.arg(opt.cmd)
+			.args(opt.args)
+			.kill_on_drop(true)
+			.spawn()?,
+	);
 }
