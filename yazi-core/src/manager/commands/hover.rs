c83963df11221c85d0527c318683e306f92babe3
sxyazi <sxyazi@gmail.com>
2023-11-23 21:35:34 +0800

    ..

--- yazi-core/src/manager/commands/hover.rs
+++ yazi-core/src/manager/commands/hover.rs
@@ -23,6 +23,10 @@ impl Manager {
 	}
 
 	pub fn hover(&mut self, opt: impl Into<Opt>) -> bool {
+		// Hover on the file
+		let opt = opt.into() as Opt;
+		let b = self.current_mut().repos(opt.url);
+
 		// Re-peek
 		self.peek(0);
 
@@ -39,8 +43,6 @@ impl Manager {
 		}
 		self.watcher.watch(to_watch);
 
-		// Hover on the file
-		let opt = opt.into() as Opt;
-		self.current_mut().repos(opt.url)
+		b
 	}
 }
