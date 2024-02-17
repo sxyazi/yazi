5d599167a19cf0eafbb6e7f51f0bf461678d12ac
sxyazi <sxyazi@gmail.com>
2024-02-17 17:20:11 +0800

    ..

--- yazi-core/src/tab/commands/leave.rs
+++ yazi-core/src/tab/commands/leave.rs
@@ -1,8 +1,6 @@
-use std::mem;
+use yazi_shared::event::Cmd;
 
-use yazi_shared::{event::Cmd, render};
-
-use crate::{manager::Manager, tab::Tab};
+use crate::tab::Tab;
 
 pub struct Opt;
 impl From<()> for Opt {
@@ -14,36 +12,12 @@ impl From<Cmd> for Opt {
 
 impl Tab {
 	pub fn leave(&mut self, _: impl Into<Opt>) {
-		let current = self
+		self
 			.current
 			.hovered()
 			.and_then(|h| h.parent())
 			.filter(|p| *p != self.current.cwd)
-			.or_else(|| self.current.cwd.parent_url());
-
-		let Some(current) = current else {
-			return;
-		};
-
-		// Parent
-		if let Some(rep) = self.parent.take() {
-			self.history.insert(rep.cwd.clone(), rep);
-		}
-		if let Some(parent) = current.parent_url() {
-			self.parent = Some(self.history_new(&parent));
-		}
-
-		// Current
-		let rep = self.history_new(&current);
-		let rep = mem::replace(&mut self.current, rep);
-		if rep.cwd.is_regular() {
-			self.history.insert(rep.cwd.clone(), rep);
-		}
-
-		// Backstack
-		self.backstack.push(current);
-
-		Manager::_refresh();
-		render!();
+			.or_else(|| self.current.cwd.parent_url())
+			.map(|u| self.cd(u));
 	}
 }
