ededc70932b248da6436f2b1c5f1e2d8b4a54ca8
sxyazi <sxyazi@gmail.com>
2024-04-25 16:36:54 +0800

    feat: support expanding Windows paths like "D:" that only have a drive letter but no root

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,9 @@ cargo publish -p yazi-config
 cargo publish -p yazi-proxy
 cargo publish -p yazi-adaptor
 cargo publish -p yazi-boot
+cargo publish -p yazi-dds
 cargo publish -p yazi-scheduler
 cargo publish -p yazi-plugin
 cargo publish -p yazi-core
 cargo publish -p yazi-fm
+cargo publish -p yazi-cli
