b540d103ab0f26df803238fabc53071ac8398353
sxyazi <sxyazi@gmail.com>
2024-05-26 19:29:17 +0800

    Simplify the code

--- yazi-config/src/keymap/run.rs
+++ yazi-config/src/keymap/run.rs
@@ -1,41 +1,14 @@
-use std::{fmt, mem};
+use std::{fmt, str::FromStr};
 
-use anyhow::{bail, Result};
+use anyhow::Result;
 use serde::{de::{self, Visitor}, Deserializer};
-use yazi_shared::event::{Cmd, Data};
+use yazi_shared::event::Cmd;
 
 pub(super) fn run_deserialize<'de, D>(deserializer: D) -> Result<Vec<Cmd>, D::Error>
 where
 	D: Deserializer<'de>,
 {
 	struct RunVisitor;
-
-	#[allow(clippy::explicit_counter_loop)]
-	fn parse(s: &str) -> Result<Cmd> {
-		let mut args = shell_words::split(s)?;
-		let mut cmd = Cmd { name: mem::take(&mut args[0]), ..Default::default() };
-
-		let mut i = 0usize;
-		for arg in args.into_iter().skip(1) {
-			let Some(arg) = arg.strip_prefix("--") else {
-				cmd.args.insert(i.to_string(), Data::String(arg));
-				i += 1;
-				continue;
-			};
-
-			let mut parts = arg.splitn(2, '=');
-			let Some(key) = parts.next().map(|s| s.to_owned()) else {
-				bail!("invalid argument: {arg}");
-			};
-
-			if let Some(val) = parts.next() {
-				cmd.args.insert(key, Data::String(val.to_owned()));
-			} else {
-				cmd.args.insert(key, Data::Boolean(true));
-			}
-		}
-		Ok(cmd)
-	}
 
 	impl<'de> Visitor<'de> for RunVisitor {
 		type Value = Vec<Cmd>;
@@ -50,7 +23,7 @@ where
 		{
 			let mut cmds = vec![];
 			while let Some(value) = &seq.next_element::<String>()? {
-				cmds.push(parse(value).map_err(de::Error::custom)?);
+				cmds.push(Cmd::from_str(value).map_err(de::Error::custom)?);
 			}
 			if cmds.is_empty() {
 				return Err(de::Error::custom("`run` within keymap.toml cannot be empty"));
@@ -62,7 +35,7 @@ where
 		where
 			E: de::Error,
 		{
-			Ok(vec![parse(value).map_err(de::Error::custom)?])
+			Ok(vec![Cmd::from_str(value).map_err(de::Error::custom)?])
 		}
 	}
 
--- yazi-core/src/tab/commands/sort.rs
+++ yazi-core/src/tab/commands/sort.rs
@@ -4,40 +4,20 @@ use yazi_config::manager::SortBy;
 use yazi_proxy::ManagerProxy;
 use yazi_shared::event::Cmd;
 
-use crate::{tab::{Config, Tab}, tasks::Tasks};
+use crate::{tab::Tab, tasks::Tasks};
 
 impl Tab {
 	pub fn sort(&mut self, mut c: Cmd, tasks: &Tasks) {
-		let defaults = Config::default();
+		let conf = &mut self.conf;
 		if let Some(by) = c.take_first_str() {
-			self.conf.sort_by = SortBy::from_str(&by).unwrap_or_default();
-		} else {
-			self.conf.sort_by = defaults.sort_by;
+			conf.sort_by = SortBy::from_str(&by).unwrap_or_default();
 		}
 
-		if let Some(rev) = c.maybe_bool("reverse") {
-			self.conf.sort_reverse = rev;
-		} else {
-			self.conf.sort_reverse = defaults.sort_reverse;
-		}
-
-		if let Some(d_f) = c.maybe_bool("dir-first") {
-			self.conf.sort_dir_first = d_f;
-		} else {
-			self.conf.sort_dir_first = defaults.sort_dir_first;
-		}
-
-		if let Some(sen) = c.maybe_bool("sensitive") {
-			self.conf.sort_sensitive = sen;
-		} else {
-			self.conf.sort_sensitive = defaults.sort_sensitive;
-		}
-
-		if let Some(tran) = c.maybe_bool("transliteration") {
-			self.conf.sort_transliteration = tran;
-		} else {
-			self.conf.sort_transliteration = defaults.sort_transliteration;
-		}
+		conf.sort_reverse = c.maybe_bool("reverse").unwrap_or(conf.sort_reverse);
+		conf.sort_dir_first = c.maybe_bool("dir-first").unwrap_or(conf.sort_dir_first);
+		conf.sort_sensitive = c.maybe_bool("sensitive").unwrap_or(conf.sort_sensitive);
+		conf.sort_transliteration =
+			c.maybe_bool("transliteration").unwrap_or(conf.sort_transliteration);
 
 		self.apply_files_attrs();
 		ManagerProxy::update_paged();
