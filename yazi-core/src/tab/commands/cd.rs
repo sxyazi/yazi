6d3fa28bfb668485ae2aec5dddbec98c9e94cbee
Shupeng Xue <dspxue@gmail.com>
2024-06-23 19:09:25 +1000

    Fix: add hover dds when cd

--- yazi-core/src/tab/commands/cd.rs
+++ yazi-core/src/tab/commands/cd.rs
@@ -66,6 +66,7 @@ impl Tab {
 		}
 
 		Pubsub::pub_from_cd(self.idx, &self.current.cwd);
+		Pubsub::pub_from_hover(self.idx, self.current.hovered().map(|h| &h.url));
 		ManagerProxy::refresh();
 		render!();
 	}
