a94bd8a3c3729f2116842f00d97fd0425e8b0a4e
uncenter <47499684+uncenter@users.noreply.github.com>
2024-07-31 20:44:57 -0400

    style(nix): replace nixpkgs-fmt with nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -11,47 +11,51 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }@inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }@inputs:
     let
       # Nixpkgs overlays
       overlays = [
         rust-overlay.overlays.default
         (final: _: {
-          rustToolchain = final.rust-bin.stable.latest.default.override {
-            extensions = [ "rust-src" ];
-          };
+          rustToolchain = final.rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; };
         })
       ];
     in
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs { inherit system overlays; };
-          rev = self.shortRev or "dirty";
-          date = self.lastModifiedDate or self.lastModified or "19700101";
-          version = (builtins.fromTOML
-            (builtins.readFile ./yazi-fm/Cargo.toml)).package.version
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system overlays; };
+        rev = self.shortRev or "dirty";
+        date = self.lastModifiedDate or self.lastModified or "19700101";
+        version =
+          (builtins.fromTOML (builtins.readFile ./yazi-fm/Cargo.toml)).package.version
           + "pre${builtins.substring 0 8 date}_${rev}";
 
-          yazi-unwrapped = pkgs.callPackage ./nix/yazi-unwrapped.nix { inherit version rev date; };
-          yazi = pkgs.callPackage ./nix/yazi.nix { inherit yazi-unwrapped; };
-        in
-        {
-          packages = {
-            inherit yazi-unwrapped yazi;
-            default = yazi;
-          };
+        yazi-unwrapped = pkgs.callPackage ./nix/yazi-unwrapped.nix { inherit version rev date; };
+        yazi = pkgs.callPackage ./nix/yazi.nix { inherit yazi-unwrapped; };
+      in
+      {
+        packages = {
+          inherit yazi-unwrapped yazi;
+          default = yazi;
+        };
 
-          formatter = pkgs.nixpkgs-fmt;
+        formatter = pkgs.nixfmt-rfc-style;
 
-          devShells.default = import ./nix/shell.nix { inherit pkgs inputs; };
-        })
+        devShells.default = import ./nix/shell.nix { inherit pkgs inputs; };
+      }
+    )
     // {
       overlays = rec {
         default = yazi;
-        yazi = final: _: {
-          inherit (self.packages."${final.system}") yazi yazi-unwrapped;
-        };
+        yazi = final: _: { inherit (self.packages."${final.system}") yazi yazi-unwrapped; };
       };
     };
 }
