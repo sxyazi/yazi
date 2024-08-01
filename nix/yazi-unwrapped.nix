c7b002581a14f870a6d36292766e185ebad43b51
uncenter <47499684+uncenter@users.noreply.github.com>
2024-07-31 23:01:07 -0400

    refactor(nix): cleanup and switch to `nixfmt-rfc-style` (#1376)
    Co-authored-by: Isabel <isabel@isabelroses.com>

--- nix/yazi-unwrapped.nix
+++ nix/yazi-unwrapped.nix
@@ -1,59 +1,67 @@
-{ makeRustPlatform
-, rustToolchain
-, version ? "git"
-, rev
-, date
-, lib
+{
+  makeRustPlatform,
+  rustToolchain,
+  version ? "git",
+  rev,
+  date,
+  lib,
 
-, installShellFiles
-, stdenv
-, darwin
+  installShellFiles,
+  stdenv,
+  darwin,
 
-, imagemagick
+  imagemagick,
 }:
 
-(makeRustPlatform { cargo = rustToolchain; rustc = rustToolchain; }).buildRustPackage rec {
-  pname = "yazi";
-  inherit version rev;
+(makeRustPlatform {
+  cargo = rustToolchain;
+  rustc = rustToolchain;
+}).buildRustPackage
+  {
+    pname = "yazi";
+    inherit version;
 
-  src = ../.;
+    src = ../.;
 
-  cargoLock = {
-    lockFile = ../Cargo.lock;
-    outputHashes = {
-      "notify-6.1.1" = "sha256-5Ft2yvRPi2EaErcGBkF/3Xv6K7ijFGbdjmSqI4go/h4=";
+    cargoLock = {
+      lockFile = ../Cargo.lock;
+      outputHashes = {
+        "notify-6.1.1" = "sha256-5Ft2yvRPi2EaErcGBkF/3Xv6K7ijFGbdjmSqI4go/h4=";
+      };
     };
-  };
 
-  env.YAZI_GEN_COMPLETIONS = true;
-  env.VERGEN_GIT_SHA = rev;
-  env.VERGEN_BUILD_DATE = builtins.concatStringsSep "-" (builtins.match "(.{4})(.{2})(.{2}).*" date);
+    env = {
+      YAZI_GEN_COMPLETIONS = true;
+      VERGEN_GIT_SHA = rev;
+      VERGEN_BUILD_DATE = builtins.concatStringsSep "-" (builtins.match "(.{4})(.{2})(.{2}).*" date);
+    };
 
-  nativeBuildInputs = [ installShellFiles imagemagick ];
-  buildInputs = lib.optionals stdenv.isDarwin (
-    with darwin.apple_sdk.frameworks; [ Foundation ]
-  );
+    nativeBuildInputs = [
+      installShellFiles
+      imagemagick
+    ];
+    buildInputs = lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [ Foundation ]);
 
-  postInstall = ''
-    installShellCompletion --cmd yazi \
-      --bash ./yazi-boot/completions/yazi.bash \
-      --fish ./yazi-boot/completions/yazi.fish \
-      --zsh  ./yazi-boot/completions/_yazi
+    postInstall = ''
+      installShellCompletion --cmd yazi \
+        --bash ./yazi-boot/completions/yazi.bash \
+        --fish ./yazi-boot/completions/yazi.fish \
+        --zsh  ./yazi-boot/completions/_yazi
 
-    # Resize logo
-    for RES in 16 24 32 48 64 128 256; do
-      mkdir -p $out/share/icons/hicolor/"$RES"x"$RES"/apps
-      magick assets/logo.png -resize "$RES"x"$RES" $out/share/icons/hicolor/"$RES"x"$RES"/apps/yazi.png
-    done
+      # Resize logo
+      for RES in 16 24 32 48 64 128 256; do
+        mkdir -p $out/share/icons/hicolor/"$RES"x"$RES"/apps
+        magick assets/logo.png -resize "$RES"x"$RES" $out/share/icons/hicolor/"$RES"x"$RES"/apps/yazi.png
+      done
 
-    mkdir -p $out/share/applications
-    install -m644 assets/yazi.desktop $out/share/applications/
-  '';
+      mkdir -p $out/share/applications
+      install -m644 assets/yazi.desktop $out/share/applications/
+    '';
 
-  meta = with lib; {
-    description = "Blazing fast terminal file manager written in Rust, based on async I/O";
-    homepage = "https://github.com/sxyazi/yazi";
-    license = licenses.mit;
-    mainProgram = "yazi";
-  };
-}
+    meta = {
+      description = "Blazing fast terminal file manager written in Rust, based on async I/O";
+      homepage = "https://github.com/sxyazi/yazi";
+      license = lib.licenses.mit;
+      mainProgram = "yazi";
+    };
+  }
