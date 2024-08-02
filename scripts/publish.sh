9b7821a1611c5578ed354cd1d5ea2a57f042160f
Mark Zaytsev <markzaytsevdev@gmail.com>
2024-08-02 14:20:14 +0700

    feat: add filtering tip in help component (#1361)
    Co-authored-by: sxyazi <sxyazi@gmail.com>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,7 @@
 cargo publish -p yazi-shared
 cargo publish -p yazi-config
 cargo publish -p yazi-proxy
+cargo publish -p yazi-fs
 cargo publish -p yazi-adapter
 cargo publish -p yazi-boot
 cargo publish -p yazi-dds
