2618c9c2725e59ebe5be7f028963f4b6ccbf2182
XYenon <i@xyenon.bid>
2024-08-10 12:03:29 +0800

    nix: update dependencies

--- nix/yazi.nix
+++ nix/yazi.nix
@@ -4,31 +4,23 @@
   runCommand,
   makeWrapper,
 
-  extraPackages ? [ ],
-  optionalDeps ? [
-    jq
-    poppler_utils
-    unar
-    ffmpegthumbnailer
-    fd
-    ripgrep
-    fzf
-    zoxide
-  ],
+  runtimeDeps ? (ps: ps),
 
   # deps
   file,
   yazi-unwrapped,
 
-  # optional deps
+  # default optional deps
   jq,
   poppler_utils,
-  unar,
+  _7zz,
   ffmpegthumbnailer,
   fd,
   ripgrep,
   fzf,
   zoxide,
+  imagemagick,
+  chafa,
 
   settings ? { },
   plugins ? { },
@@ -44,7 +36,19 @@ let
     mapAttrsToList
     ;
 
-  runtimePaths = [ file ] ++ optionalDeps ++ extraPackages;
+  defaultDeps = [
+    jq
+    poppler_utils
+    _7zz
+    ffmpegthumbnailer
+    fd
+    ripgrep
+    fzf
+    zoxide
+    imagemagick
+    chafa
+  ];
+  runtimePaths = [ file ] ++ (runtimeDeps defaultDeps);
 
   settingsFormat = formats.toml { };
 
