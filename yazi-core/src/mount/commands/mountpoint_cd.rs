6cc018cb22a7c7db2b73425488b691de78de0c91
Lutsay Aleksandr Valeryevich <alutsay@ozon.ru>
2024-12-01 02:07:19 +0300

    create mountpoint_cd command

--- yazi-core/src/mount/commands/mountpoint_cd.rs
+++ yazi-core/src/mount/commands/mountpoint_cd.rs
@@ -1,9 +1,14 @@
+use yazi_macro::emit;
 use yazi_proxy::options::ProcessExecOpt;
+use yazi_shared::{Layer, event::Cmd, fs::Url};
 
 use crate::mount::Mount;
 
 impl Mount {
-	pub fn mountpoint_cd(&mut self, opt: impl TryInto<ProcessExecOpt>) {
-		if let Ok(opt) = opt.try_into() {}
+	pub fn mountpoint_cd(&mut self, _opt: impl TryInto<ProcessExecOpt>) {
+		if let Some(target) = self.points.get(self.cursor) {
+			let url: Url = target.path.clone().into();
+			emit!(Call(Cmd::args("cd", &[url]), Layer::Manager));
+		}
 	}
 }
