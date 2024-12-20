783e95b1f2a61ef8405d016ce2745511d9b67753
sxyazi <sxyazi@gmail.com>
2024-12-20 22:46:24 +0800

    chore: bump version to 0.4.3

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,27 +1,14 @@
-cargo publish -p yazi-macro
-sleep 30
-cargo publish -p yazi-codegen
-sleep 30
-cargo publish -p yazi-shared
-sleep 30
-cargo publish -p yazi-fs
-sleep 30
-cargo publish -p yazi-config
-sleep 30
-cargo publish -p yazi-proxy
-sleep 30
-cargo publish -p yazi-adapter
-sleep 30
-cargo publish -p yazi-boot
-sleep 30
-cargo publish -p yazi-dds
-sleep 30
-cargo publish -p yazi-scheduler
-sleep 30
-cargo publish -p yazi-plugin
-sleep 30
-cargo publish -p yazi-core
-sleep 30
-cargo publish -p yazi-fm
-sleep 30
+cargo publish -p yazi-macro && sleep 30
+cargo publish -p yazi-codegen && sleep 30
+cargo publish -p yazi-shared && sleep 30
+cargo publish -p yazi-fs && sleep 30
+cargo publish -p yazi-config && sleep 30
+cargo publish -p yazi-proxy && sleep 30
+cargo publish -p yazi-adapter && sleep 30
+cargo publish -p yazi-boot && sleep 30
+cargo publish -p yazi-dds && sleep 30
+cargo publish -p yazi-scheduler && sleep 30
+cargo publish -p yazi-plugin && sleep 30
+cargo publish -p yazi-core && sleep 30
+cargo publish -p yazi-fm && sleep 30
 cargo publish -p yazi-cli
