db5823681faff0e6a3efdcb0c123410e4fe44cc9
Guangxiong Lin <hi@gxlin.org>
2025-01-10 00:03:11 +0800

    Forward close params to quit

--- yazi-core/src/manager/commands/close.rs
+++ yazi-core/src/manager/commands/close.rs
@@ -3,10 +3,10 @@ use yazi_shared::event::CmdCow;
 use crate::{manager::Manager, tasks::Tasks};
 
 impl Manager {
-	pub fn close(&mut self, _: CmdCow, tasks: &Tasks) {
+	pub fn close(&mut self, c: CmdCow, tasks: &Tasks) {
 		if self.tabs.len() > 1 {
 			return self.tabs.close(self.tabs.cursor);
 		}
-		self.quit((), tasks);
+		self.quit(c, tasks);
 	}
 }
