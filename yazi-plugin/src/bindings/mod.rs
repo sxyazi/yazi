7a75f38e8ec648e51bce7e82786db5cb6c135a05
sxyazi <sxyazi@gmail.com>
2025-01-17 01:03:41 +0800

    feat: channel and multi-concurrent task join support for the plugin system

--- yazi-plugin/src/bindings/mod.rs
+++ yazi-plugin/src/bindings/mod.rs
@@ -1,3 +1,3 @@
 #![allow(clippy::module_inception)]
 
-yazi_macro::mod_flat!(bindings cha icon image input mouse permit range window);
+yazi_macro::mod_flat!(bindings cha chan icon image input mouse permit range window);
