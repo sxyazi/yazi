1f4d0eafb5e5ec38a4e148271c2f37f52fb9a846
三咲雅 · Misaki Masa <sxyazi@gmail.com>
2025-01-22 20:34:14 +0800

    fix: unmangle the hexadecimal space strings (`"\x20"`) in Linux partition labels (#2233)

--- yazi-fs/src/mounts/linux.rs
+++ yazi-fs/src/mounts/linux.rs
@@ -1,9 +1,9 @@
-use std::{borrow::Cow, collections::{HashMap, HashSet}, ffi::{OsStr, OsString}, os::{fd::AsFd, unix::{ffi::OsStrExt, fs::MetadataExt}}, time::Duration};
+use std::{borrow::Cow, collections::{HashMap, HashSet}, ffi::{OsStr, OsString}, os::{fd::AsFd, unix::{ffi::{OsStrExt, OsStringExt}, fs::MetadataExt}}, time::Duration};
 
 use anyhow::Result;
 use tokio::{io::{Interest, unix::AsyncFd}, time::sleep};
 use tracing::error;
-use yazi_shared::replace_cow;
+use yazi_shared::{replace_cow, replace_vec_cow};
 
 use super::{Locked, Partition, Partitions};
 
@@ -129,7 +129,14 @@ impl Partitions {
 		let mut map = HashMap::new();
 		for entry in std::fs::read_dir("/dev/disk/by-label")?.flatten() {
 			let meta = std::fs::metadata(entry.path())?;
-			map.insert((meta.dev(), meta.ino()), entry.file_name());
+			let name = entry.file_name();
+			map.insert(
+				(meta.dev(), meta.ino()),
+				match replace_vec_cow(name.as_bytes(), br"\x20", b" ") {
+					Cow::Borrowed(_) => name,
+					Cow::Owned(v) => OsString::from_vec(v),
+				},
+			);
 		}
 		Ok(map)
 	}
