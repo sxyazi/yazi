8b5331b7817084bd8aa6cf353f62a51232a7c2aa
sxyazi <sxyazi@gmail.com>
2025-02-24 00:34:08 +0800

    feat: new `cf.args` to access startup arguments

--- yazi-plugin/src/config/config.rs
+++ yazi-plugin/src/config/config.rs
@@ -1,7 +1,8 @@
 use mlua::{IntoLua, Lua, LuaSerdeExt, SerializeOptions, Value};
+use yazi_boot::ARGS;
 use yazi_config::{MANAGER, PREVIEW, THEME};
 
-use crate::Composer;
+use crate::{Composer, url::Url};
 
 pub const OPTS: SerializeOptions =
 	SerializeOptions::new().serialize_none_to_null(false).serialize_unit_to_null(false);
@@ -14,9 +15,20 @@ impl<'a> Config<'a> {
 	pub fn compose(lua: &Lua) -> mlua::Result<Value> {
 		Composer::make(lua, 5, |lua, key| {
 			match key {
+				b"args" => Self::args(lua)?,
 				b"manager" => Self::manager(lua)?,
 				b"plugin" => super::Plugin::compose(lua)?,
 				b"preview" => Self::preview(lua)?,
+				_ => return Ok(Value::Nil),
+			}
+			.into_lua(lua)
+		})
+	}
+
+	fn args(lua: &Lua) -> mlua::Result<Value> {
+		Composer::make(lua, 5, |lua, key| {
+			match key {
+				b"chooser_file" => ARGS.chooser_file.as_ref().map(Url::from).into_lua(lua)?,
 				_ => return Ok(Value::Nil),
 			}
 			.into_lua(lua)
