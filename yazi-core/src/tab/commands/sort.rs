cbe614f23ce5a5d0b1272cf0b10725768df8e3b8
sxyazi <sxyazi@gmail.com>
2025-05-05 01:16:36 +0800

    fix: missing a `hover` event after `cd`

--- yazi-core/src/tab/commands/sort.rs
+++ yazi-core/src/tab/commands/sort.rs
@@ -8,16 +8,18 @@ use crate::{tab::Tab, tasks::Tasks};
 
 impl Tab {
 	pub fn sort(&mut self, c: CmdCow, tasks: &Tasks) {
-		let pref = &mut self.pref;
-		if let Some(by) = c.first_str() {
-			pref.sort_by = SortBy::from_str(by).unwrap_or_default();
+		let mut new = self.pref.clone();
+		new.sort_by = c.first_str().and_then(|s| SortBy::from_str(s).ok()).unwrap_or(new.sort_by);
+		new.sort_reverse = c.maybe_bool("reverse").unwrap_or(new.sort_reverse);
+		new.sort_dir_first = c.maybe_bool("dir-first").unwrap_or(new.sort_dir_first);
+		new.sort_sensitive = c.maybe_bool("sensitive").unwrap_or(new.sort_sensitive);
+		new.sort_translit = c.maybe_bool("translit").unwrap_or(new.sort_translit);
+
+		if new == self.pref {
+			return;
 		}
 
-		pref.sort_reverse = c.maybe_bool("reverse").unwrap_or(pref.sort_reverse);
-		pref.sort_dir_first = c.maybe_bool("dir-first").unwrap_or(pref.sort_dir_first);
-		pref.sort_sensitive = c.maybe_bool("sensitive").unwrap_or(pref.sort_sensitive);
-		pref.sort_translit = c.maybe_bool("translit").unwrap_or(pref.sort_translit);
-
+		self.pref = new;
 		self.apply_files_attrs();
 		self.hover(None);
 		tasks.prework_sorted(&self.current.files);
