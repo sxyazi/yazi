c52f055b452e4da04cf4ea498db5303e3902ade3
sxyazi <sxyazi@gmail.com>
2025-05-17 14:18:47 +0800

    fix: always try to create state directory before draining DDS data

--- yazi-fm/src/app/commands/quit.rs
+++ yazi-fm/src/app/commands/quit.rs
@@ -1,5 +1,6 @@
 use std::ffi::OsString;
 
+use tokio::fs;
 use yazi_boot::ARGS;
 use yazi_shared::event::EventQuit;
 
@@ -9,29 +10,29 @@ impl App {
 	pub(crate) fn quit(&mut self, opt: EventQuit) -> ! {
 		self.cx.tasks.shutdown();
 		self.cx.mgr.shutdown();
-		futures::executor::block_on(yazi_dds::shutdown());
-		futures::executor::block_on(yazi_dds::STATE.drain()).ok();
 
-		if !opt.no_cwd_file {
-			self.cwd_to_file();
-		}
-		if let Some(selected) = opt.selected {
-			self.selected_to_file(selected);
-		}
+		futures::executor::block_on(async {
+			_ = futures::join!(
+				yazi_dds::shutdown(),
+				yazi_dds::STATE.drain(),
+				self.cwd_to_file(opt.no_cwd_file),
+				self.selected_to_file(opt.selected)
+			);
+		});
 
 		Term::goodbye(|| opt.code);
 	}
 
-	fn cwd_to_file(&self) {
-		if let Some(p) = &ARGS.cwd_file {
+	async fn cwd_to_file(&self, no: bool) {
+		if let Some(p) = ARGS.cwd_file.as_ref().filter(|_| !no) {
 			let cwd = self.cx.mgr.cwd().as_os_str();
-			std::fs::write(p, cwd.as_encoded_bytes()).ok();
+			fs::write(p, cwd.as_encoded_bytes()).await.ok();
 		}
 	}
 
-	fn selected_to_file(&self, selected: OsString) {
-		if let Some(p) = &ARGS.chooser_file {
-			std::fs::write(p, selected.as_encoded_bytes()).ok();
+	async fn selected_to_file(&self, selected: Option<OsString>) {
+		if let (Some(s), Some(p)) = (selected, &ARGS.chooser_file) {
+			fs::write(p, s.as_encoded_bytes()).await.ok();
 		}
 	}
 }
