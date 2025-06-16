56a4e87d7e97ea08732079450300ba89e2c3d101
三咲雅 misaki masa <sxyazi@gmail.com>
2025-06-16 13:18:07 +0800

    fix: interoperability between number, integer, and id (#2883)

--- yazi-proxy/src/cmp.rs
+++ yazi-proxy/src/cmp.rs
@@ -4,12 +4,10 @@ use yazi_shared::{Id, event::Cmd};
 pub struct CmpProxy;
 
 impl CmpProxy {
-	#[inline]
 	pub fn close() {
 		emit!(Call(Cmd::new("cmp:close")));
 	}
 
-	#[inline]
 	pub fn trigger(word: &str, ticket: Id) {
 		emit!(Call(Cmd::args("cmp:trigger", [word]).with("ticket", ticket)));
 	}
--- yazi-proxy/src/tab.rs
+++ yazi-proxy/src/tab.rs
@@ -8,22 +8,18 @@ use crate::options::SearchOpt;
 pub struct TabProxy;
 
 impl TabProxy {
-	#[inline]
 	pub fn cd(target: &Url) {
 		emit!(Call(Cmd::args("mgr:cd", [target])));
 	}
 
-	#[inline]
 	pub fn reveal(target: &Url) {
 		emit!(Call(Cmd::args("mgr:reveal", [target]).with("no-dummy", true)));
 	}
 
-	#[inline]
 	pub fn arrow(step: impl Into<Cow<'static, str>>) {
 		emit!(Call(Cmd::args("mgr:arrow", [step.into()])));
 	}
 
-	#[inline]
 	pub fn search_do(opt: SearchOpt) {
 		emit!(Call(
 			// TODO: use second positional argument instead of `args` parameter
