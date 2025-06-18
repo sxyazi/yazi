624eb4fb256e7faa9cbce4e3ad11aa1c165ee5d0
sxyazi <sxyazi@gmail.com>
2025-06-18 19:18:50 +0800

    Remove Rust nightly API

--- yazi-macro/src/fmt.rs
+++ yazi-macro/src/fmt.rs
@@ -3,15 +3,7 @@ macro_rules! try_format {
 	($($arg:tt)*) => {{
 		use std::fmt::Write;
 
-		fn inner(args: std::fmt::Arguments<'_>) -> Result<String, std::fmt::Error> {
-			if let Some(s) = args.as_str() {
-				Ok(s.to_owned())
-			} else {
-				let mut output = String::new();
-				output.write_fmt(args).map(|_| output)
-			}
-		}
-
-		inner(format_args!($($arg)*))
+		let mut output = String::new();
+		output.write_fmt(format_args!($($arg)*)).map(|_| output)
 	}}
 }
