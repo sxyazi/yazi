8ef28ec6786fe4770ee302f7323f87775109a0c3
三咲雅 misaki masa <sxyazi@gmail.com>
2025-06-27 23:03:16 +0800

    refactor: deprecate `ui.Rect.default` (#2927)

--- yazi-plugin/src/elements/rect.rs
+++ yazi-plugin/src/elements/rect.rs
@@ -3,6 +3,7 @@ use std::ops::Deref;
 use mlua::{FromLua, IntoLua, Lua, MetaMethod, Table, UserData, Value};
 
 use super::Pad;
+use crate::deprecate;
 
 #[derive(Clone, Copy, Debug, Default, FromLua)]
 pub struct Rect(pub(super) ratatui::layout::Rect);
@@ -34,9 +35,20 @@ impl Rect {
 			}))
 		})?;
 
-		let rect = lua.create_table_from([("default", Self(ratatui::layout::Rect::default()))])?;
+		let index = lua.create_function(move |lua, (_, key): (Table, mlua::String)| {
+			Ok(match key.as_bytes().as_ref() {
+				b"default" => {
+					deprecate!(lua, "`ui.Rect.default` is deprecated, use `ui.Rect{{}}` instead, in your {}\nSee #2927 for more details: https://github.com/sxyazi/yazi/pull/2927");
+					Some(Self(Default::default()))
+				}
+				_ => None,
+			})
+		})?;
 
-		rect.set_metatable(Some(lua.create_table_from([(MetaMethod::Call.name(), new)])?));
+		let rect = lua.create_table()?;
+		rect.set_metatable(Some(
+			lua.create_table_from([(MetaMethod::Call.name(), new), (MetaMethod::Index.name(), index)])?,
+		));
 		rect.into_lua(lua)
 	}
 
