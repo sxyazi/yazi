895bd3c88858805f940b2747f0a647a05ce554aa
sxyazi <sxyazi@gmail.com>
2026-01-01 09:17:48 +0800

    Add changelog

--- yazi-cli/src/package/mod.rs
+++ yazi-cli/src/package/mod.rs
@@ -4,13 +4,13 @@ use anyhow::Context;
 use yazi_fs::Xdg;
 
 pub(super) fn init() -> anyhow::Result<()> {
-	let packages_root = Xdg::state_dir().join("packages");
-	std::fs::create_dir_all(&packages_root)
-		.with_context(|| format!("failed to create packages directory: {packages_root:?}"))?;
+	let packages_dir = Xdg::state_dir().join("packages");
+	std::fs::create_dir_all(&packages_dir)
+		.with_context(|| format!("failed to create packages directory: {packages_dir:?}"))?;
 
-	let config_root = Xdg::config_dir();
-	std::fs::create_dir_all(&config_root)
-		.with_context(|| format!("failed to create config directory: {config_root:?}"))?;
+	let config_dir = Xdg::config_dir();
+	std::fs::create_dir_all(&config_dir)
+		.with_context(|| format!("failed to create config directory: {config_dir:?}"))?;
 
 	Ok(())
 }
