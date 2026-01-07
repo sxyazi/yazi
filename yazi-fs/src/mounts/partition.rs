30ac041d9e9a42f9157d279300c694a2b1ed6ad8
Gabriel Wicke <gswicke@gmail.com>
2026-01-07 10:25:50 -0800

    refactor: make partition heuristic predicate a positive match (#3526)

--- yazi-fs/src/mounts/partition.rs
+++ yazi-fs/src/mounts/partition.rs
@@ -14,9 +14,11 @@ pub struct Partition {
 }
 
 impl Partition {
+	// Match mount types that do not reliably emit change notifications, or update directory
+	// metadata on changes, and should be refreshed frequently / heuristically.
 	pub fn heuristic(&self) -> bool {
 		let b: &[u8] = self.fstype.as_ref().map_or(b"", |s| s.as_encoded_bytes());
-		!matches!(b, b"exfat" | b"fuse.rclone")
+		matches!(b, b"exfat" | b"fuse.rclone")
 	}
 
 	#[rustfmt::skip]
