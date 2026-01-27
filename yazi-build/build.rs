71e7293126c5f82a844217707241e1551fdba101
sxyazi <sxyazi@gmail.com>
2026-01-27 08:48:49 +0800

    fix: ignore `target-dir` to avoid deadlocks during `cargo install`

--- yazi-build/build.rs
+++ yazi-build/build.rs
@@ -17,6 +17,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 	let target = env::var("TARGET").unwrap();
 	let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
 	unsafe {
+		env::set_var("CARGO_TARGET_DIR", "target");
 		env::set_var("VERGEN_GIT_SHA", "Crates.io");
 		env::set_var("YAZI_CRATE_BUILD", "1");
 
