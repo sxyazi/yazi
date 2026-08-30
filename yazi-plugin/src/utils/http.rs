af43f09abe6f2f8e35b19b691f95755ec7b5df5e
三咲雅 misaki masa <sxyazi@gmail.com>
2026-08-31 07:59:06 +0800

    fix: scope Lua HTTP client initialization error (#4308)

--- yazi-plugin/src/utils/http.rs
+++ yazi-plugin/src/utils/http.rs
@@ -1,8 +1,10 @@
+use std::sync::OnceLock;
+
 use mlua::{ExternalError, Function, IntoLuaMulti, Lua, Table, Value};
+use reqwest::Client;
 use yazi_binding::{HttpRequest, HttpTransport};
 
 use super::Utils;
-use crate::HTTP;
 
 impl Utils {
 	pub(super) fn http(lua: &Lua) -> mlua::Result<Table> {
@@ -11,10 +13,28 @@ impl Utils {
 
 	fn request(lua: &Lua) -> mlua::Result<Function> {
 		lua.create_async_function(|lua, request: HttpRequest| async move {
-			match HttpTransport::new(&HTTP).send(request).await {
+			let result = match Self::client() {
+				Ok(client) => HttpTransport::new(client).send(request).await,
+				Err(e) => return (Value::Nil, e).into_lua_multi(&lua),
+			};
+
+			match result {
 				Ok(response) => response.into_lua_multi(&lua),
 				Err(e) => (Value::Nil, e.into_lua_err()).into_lua_multi(&lua),
 			}
 		})
+	}
+
+	fn client() -> mlua::Result<&'static Client> {
+		static HTTP: OnceLock<Result<Client, reqwest::Error>> = OnceLock::new();
+
+		HTTP
+		.get_or_init(|| {
+			Client::builder()
+				.user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36")
+				.build()
+		})
+		.as_ref()
+		.map_err(|e| e.into_lua_err())
 	}
 }
