b826293a1c422a00f29da51178ab71cc35d80961
三咲雅 misaki masa <sxyazi@gmail.com>
2026-09-16 01:51:41 +0800

    Fix CI

--- yazi-actor/src/cmp/trigger.rs
+++ yazi-actor/src/cmp/trigger.rs
@@ -98,7 +98,9 @@ mod tests {
 
 	fn compare(s: &str, parent: &str, child: &str) {
 		let (mut p, c) = Trigger::split_url(s).unwrap();
-		if let Ok(u) = p.try_strip_prefix(yazi_fs::CWD.load().as_ref()) {
+		if p.is_regular()
+			&& let Ok(u) = p.try_strip_prefix(yazi_fs::CWD.load().as_ref())
+		{
 			p = u.as_os().unwrap().into();
 		}
 		assert_eq!((p, c.to_str().unwrap()), (parent.parse().unwrap(), child));
